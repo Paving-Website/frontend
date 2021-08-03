e48469cead023f3355729be8c3237e19d08b1328
Shaun Soobagrah <shaunsoobagrah@Shauns-MacBook-Pro.local>
2021-08-03 12:25:08 +0400

    slideshow

--- app.js
+++ app.js
@@ -31,3 +31,12 @@ function isAlphabet(evt){
         evt.preventDefault();
     }
 }
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter>4){
+        counter=1;
+    }
+},5000);
+
